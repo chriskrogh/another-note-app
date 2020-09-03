95ac6635f8b89803bba4937c3def7b994f490c22
Christopher Krogh <chris.krogh@outlook.com>
2020-09-03 13:18:29 -0400

    wip: handling shared and server from dev script

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,30 +1,30 @@
 import 'reflect-metadata';
-import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
-import { buildSchema } from 'type-graphql';
-import HelloWorldResolver from 'resolvers/helloworld';
+// import express from 'express';
+// import { graphqlHTTP } from 'express-graphql';
+// import { buildSchema } from 'type-graphql';
+// import HelloWorldResolver from 'resolvers/helloworld';
 import { add } from '@relay/shared';
 
-console.log(add(2, 3));
+console.log(add(2, 44, 2));
 
-const main = async () => {
-  const schema = await buildSchema({
-    resolvers: [HelloWorldResolver],
-  });
-  const app = express();
-  const PORT = 5000;
+// const main = async () => {
+//   const schema = await buildSchema({
+//     resolvers: [HelloWorldResolver],
+//   });
+//   const app = express();
+//   const PORT = 5000;
 
-  app.use(
-    '/graphql',
-    graphqlHTTP({
-      schema,
-    }),
-  );
+//   app.use(
+//     '/graphql',
+//     graphqlHTTP({
+//       schema,
+//     }),
+//   );
 
-  app.listen(PORT);
-  console.log(
-    `Running a GraphQL API server at http://localhost:${PORT}/graphql`,
-  );
-};
+//   app.listen(PORT);
+//   console.log(
+//     `Running a GraphQL API server at http://localhost:${PORT}/graphql`,
+//   );
+// };
 
-main();
+// main();
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,1 +1,1 @@
-export const add = (a: number, b: number): number => a + b;
+export const add = (a: number, b: number, c: number): number => a + b + c;
