a06f16391777e03205d39c2f198426f497e6453c
Christopher Krogh <chris.krogh@outlook.com>
2020-09-04 08:11:58 -0400

    configured relay environment

---
+++ packages/client/src/relay/environment.ts
@@ -0,0 +1,23 @@
+import type { FetchFunction } from 'relay-runtime';
+import { Environment, Network, RecordSource, Store } from 'relay-runtime';
+
+const fetchQuery: FetchFunction = async (request, variables) => {
+  const response = await fetch('/graphql', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query: request.text,
+      variables,
+    }),
+  });
+  return response.json();
+}
+
+const environment = new Environment({
+  network: Network.create(fetchQuery),
+  store: new Store(new RecordSource()),
+});
+
+export default environment;
