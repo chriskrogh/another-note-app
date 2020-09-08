004ee11a3eab4e3e451cc48ee2d1096a36ff57a3
Christopher Krogh <chris.krogh@outlook.com>
2020-09-08 09:57:54 -0400

    wip: notes

--- packages/client/src/components/Notes/index.tsx
+++ packages/client/src/components/Notes/index.tsx
@@ -1,42 +1,20 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Note } from '@relay/shared/src/models/Note';
-import { Typography } from '@material-ui/core';
+import { graphql, useLazyLoadQuery } from 'react-relay/hooks';
+// import { Note } from '@relay/shared/src/models/Note';
 
-const notes: Note[] = [
-  {
-    title: 'test',
-    description: 'test d',
-  },
-  {
-    title: 'test 2',
-    description: 'test 2 d',
-  },
-  {
-    title: 'test 3',
-    description: 'test 3 d',
-  },
-];
-
-const useStyles = makeStyles(({ spacing, palette }) => ({
-  container: {
-    marginTop: spacing(1),
-    borderColor: palette.text.secondary,
-  },
-}));
+const getAllNotesQuery = graphql`
+  query {
+    allNotes {
+      title
+      description
+    }
+  }
+`;
 
 const Notes: React.FC = () => {
-  const classes = useStyles();
-  return (
-    <div className={classes.container}>
-      {notes.map((note) => (
-        <>
-          <Typography>{note.title}</Typography>
-          <Typography>{note.description}</Typography>
-        </>
-      ))}
-    </div>
-  );
+  const data = useLazyLoadQuery(getAllNotesQuery, {});
+  console.log(data);
+  return <div></div>;
 };
 
 export default Notes;
