f2f927f59d53cbeb3cd3290aa2d4e9ee94b73bd3
Christopher Krogh <chris.krogh@outlook.com>
2020-09-13 09:53:19 -0400

    made dev user to auth with

--- packages/server/src/routers/auth/index.ts
+++ packages/server/src/routers/auth/index.ts
@@ -2,6 +2,13 @@ import express from 'express';
 import FacebookRouter from './facebook';
 import GoogleRouter from './google';
 
+const devUser = {
+  _id: '5f5c4ca6bebdc26c2416fae1',
+  firstName: 'Christopher',
+  lastName: 'Krogh',
+  email: 'christopherm11@hotmail.com',
+};
+
 const router = express.Router();
 
 router.use('/facebook', FacebookRouter);
@@ -9,7 +16,9 @@ router.use('/google', GoogleRouter);
 
 router.get('/', (req, res) => {
   const user = req.session?.user;
-  if (user) {
+  if (process.env.NODE_ENV !== 'production') {
+    res.send(devUser);
+  } else if (user) {
     res.send(user);
   } else {
     res.sendStatus(400);
