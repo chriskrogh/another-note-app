ae973fd55da11ebaeb73c5e3dd3dce474dbd077d
Christopher Krogh <chris.krogh@outlook.com>
2020-09-13 14:24:25 -0400

    wip deploy

--- packages/server/src/config/db.ts
+++ packages/server/src/config/db.ts
@@ -1,8 +1,7 @@
 import mongoose from 'mongoose';
 
 const connect = async (callback: () => void): Promise<void> => {
-  const mongoUrl =
-    process.env.MONGO_URL || `mongodb://127.0.0.1:127.0.0.1/relay`;
+  const mongoUrl = process.env.MONGO_URL || `mongodb://127.0.0.1:27017/relay`;
 
   mongoose.set('useFindAndModify', false);
   mongoose.set('useUnifiedTopology', true);
