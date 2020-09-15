76e813decf3ea71053579cad8bd89c0b38ad0b35
Christopher Krogh <chris.krogh@outlook.com>
2020-09-15 13:07:54 -0400

    added subscription to resolver

--- packages/server/src/resolvers/Note/index.ts
+++ packages/server/src/resolvers/Note/index.ts
@@ -1,7 +1,17 @@
-import { Resolver, Query, Arg, Mutation } from 'type-graphql';
+import {
+  Resolver,
+  Query,
+  Subscription,
+  Mutation,
+  Arg,
+  PubSub,
+  PubSubEngine,
+} from 'type-graphql';
 import NoteModel, { Note } from '../../models/Note';
 import { CreateNoteInput } from '../../validators/Note/create';
 import { UpdateNoteInput } from '../../validators/Note/update';
+
+const NOTES_KEY = 'NOTES';
 
 @Resolver(Note)
 class NoteResolver {
@@ -10,27 +20,44 @@ class NoteResolver {
     return await NoteModel.find({ owner });
   }
 
+  @Subscription(() => [Note], {
+    topics: NOTES_KEY,
+    filter: ({ payload, args }) => args.owner === payload,
+  })
+  async subToMyNotes(@Arg('owner') owner: string): Promise<Note[]> {
+    return await NoteModel.find({ owner });
+  }
+
   @Mutation(() => Note)
   async createNote(
     @Arg('data') { title, description, owner }: CreateNoteInput,
+    @PubSub() pubSub: PubSubEngine,
   ): Promise<Note> {
-    return await NoteModel.create({ title, description, owner });
+    const note = await NoteModel.create({ title, description, owner });
+    await pubSub.publish(NOTES_KEY, owner);
+    return note;
   }
 
   @Mutation(() => Note, { nullable: true })
   async updateNote(
     @Arg('id') id: string,
     @Arg('data') { title, description }: UpdateNoteInput,
+    @PubSub() pubSub: PubSubEngine,
   ): Promise<void> {
     const updates = { title, description };
     if (!title) delete updates['title'];
     if (!description) delete updates['description'];
     await NoteModel.findByIdAndUpdate(id, updates);
+    await pubSub.publish(NOTES_KEY, id);
   }
 
   @Mutation(() => Note, { nullable: true })
-  async deleteNote(@Arg('id') id: string): Promise<Note | null> {
-    return await NoteModel.findByIdAndDelete(id);
+  async deleteNote(
+    @Arg('id') id: string,
+    @PubSub() pubSub: PubSubEngine,
+  ): Promise<void> {
+    await NoteModel.findByIdAndDelete(id);
+    await pubSub.publish(NOTES_KEY, id);
   }
 }
 
